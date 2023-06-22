73538b31e0077c027ab845258fb5b76783cc33cc
omartourijan <omar.tourijan.elfounti@gmail.com>
2023-06-22 16:47:57 +0200

    Interface de l'aplication et la mis en place de la base de donnes

---
+++ flask_app/project/auth.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, render_template,redirect, url_for , flash
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
+        flash('Email address already exists')
+        return redirect(url_for('auth.signup'))
+
+    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    # code to validate and add user to database goes here
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/logout')
+def logout():
+    return 'Logout'
