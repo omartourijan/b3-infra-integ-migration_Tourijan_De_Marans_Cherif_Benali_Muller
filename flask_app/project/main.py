2cffa2b448da448188bda0d9aba827481643f65c
omartourijan <omar.tourijan.elfounti@gmail.com>
2023-06-29 03:21:34 +0200

    Creation de l'authentification et acces a la session de l'utilisateur et configuraton de la deconexion de la session

--- flask_app/project/main.py
+++ flask_app/project/main.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 from . import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('/home/epsi/flask_app/project/templates')
+    return render_template('index.html')
 
 @main.route('/profile')
 def profile():
-    return render_template('/home/epsi/flask_app/project/templates')
+    return render_template('profile.html', name=current_user.name)
